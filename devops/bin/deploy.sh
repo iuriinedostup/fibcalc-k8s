6775599d88f0f9247c4ed8f7e0b368eacfbec114
Iurii Nedostup <iurii.nedostup@olx.com>
2021-12-28 16:44:32 +0100

    Define CI/CD

--- devops/bin/deploy.sh
+++ devops/bin/deploy.sh
@@ -4,24 +4,10 @@ set -e
 
 source $(dirname "$0")/utils.sh
 
-SHA=$(git rev-parse --short HEAD)
-
-msg.task 'Buidling docker images'
-docker build -t iuriinedostup/multi-client:latest -t iuriinedostup/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t iuriinedostup/multi-server:latest -t iuriinedostup/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t iuriinedostup/multi-worker:latest -t iuriinedostup/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-msg.done
-
-msg.task 'Pushing docker images'
-docker push iuriinedostup/multi-client:latest
-docker push iuriinedostup/multi-server:latest
-docker push iuriinedostup/multi-worker:latest
-docker push iuriinedostup/multi-client:$SHA
-docker push iuriinedostup/multi-server:$SHA
-docker push iuriinedostup/multi-worker:$SHA
-msg.done
+SHA=$SHA:-$(git rev-parse --short HEAD)
 
 aws eks update-kubeconfig --region $CLUSTER_REGION --name $CLUSTER_NAME
+export KUBECONFIG=kubeconfig
 
 msg.task 'Applyting kubernetes configs'
 kubectl apply -f ./devops/k8s/
@@ -36,4 +22,3 @@ msg.task 'Done'
 
 kubectl get pods
 echo "Deploy succeded"
-
